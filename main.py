50d6a8c05a1399d0da68f6469427330ff3436bcd
branko-bit <brankomajsen@gmail.com>
2025-03-26 16:10:08 +0100

    Added calibration function which will serve as a preprocessing function where we will capture our object for color range determination. For now the function only conatins input params and camera testing code.

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ def prikazi_skatle(slika, seznam_skatel, sirina_skatle, visina_skatle):
 def prestej_piksle_z_barvo_koze(slika, barva_koze):
     return
 
+def zajemi_kalibracijsko_sliko(sirina_kamere, visina_kamere, levo_zg_x, levo_zg_y, desno_sp_x, desno_sp_y):
+    kamera = cv2.VideoCapture(0)
+    kamera.set(cv2.CAP_PROP_FRAME_WIDTH, sirina_kamere)
+    kamera.set(cv2.CAP_PROP_FRAME_HEIGHT, visina_kamere)
+
+    #testno izpisovanje da vidim kaksne so dejanske dimenzije...ker ce mi jih kamera ne podpira gre na neke default nastavitve
+    actual_width = int(kamera.get(cv2.CAP_PROP_FRAME_WIDTH))
+    actual_height = int(kamera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("Širina: ", actual_width)
+    print("Višina: ", actual_height)
+
+    kamera.release()
+    cv2.destroyAllWindows()
+    
+    return 
+
 def main():
     return
 
