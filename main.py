576ceea0b71ffeb7a564164bcd1b9b2a547108de
branko-bit <brankomajsen@gmail.com>
2025-03-26 16:04:33 +0100

    Added function definitions (names and paramters)

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import time
+from collections import Counter
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    return
+
+def zmanjsaj_sliko(slika, sirina, visina):
+    return
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze):
+    return
+
+def prikazi_skatle(slika, seznam_skatel, sirina_skatle, visina_skatle):
+    return
+
+def prestej_piksle_z_barvo_koze(slika, barva_koze):
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
